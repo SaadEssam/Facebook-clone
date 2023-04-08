930f11cc4c9c91319783ecae61800e6174c7a022
SaadEssam <SaadEssam059@gmail.com>
2023-04-09 00:42:47 +0200

    build a Center Header Icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,43 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/outline";
+
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsPlayBtn } from "react-icons/bs";
+import { HiOutlineUserGroup, HiHome } from "react-icons/hi";
+import { BiStore } from "react-icons/bi";
+import { IoLogoGameControllerB } from "react-icons/io";
+import HeaderIcons from "./HeaderIcons";
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Leftside */}
       <div className="flex items-center">
         <Image
           src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png"
           alt="Facebook logo"
-          width={50}
-          height={50}
+          width={70}
+          height={70}
           layout="fixed"
         />
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
-          <SearchIcon className="h-6 text-gray-500" />
+          {/* <SearchIcon className="h-6 text-gray-500" /> */}
+          <AiOutlineSearch className="h-6 text-gray-500" size="1.5rem" />
           <input
             type="text"
-            className="flex ml-2 items-center bg-transparent focus:outline-none placeholder-gray-500"
+            className="flex ml-2 items-center bg-transparent focus:outline-none placeholder-gray-500 flex-shrink"
             placeholder="Search Facebook"
           />
         </div>
       </div>
       {/* Center */}
-      <div>
-        
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcons Icon={HiHome} active={true} />
+          <HeaderIcons Icon={BsPlayBtn} active={false} />
+          <HeaderIcons Icon={BiStore} active={false} />
+          <HeaderIcons Icon={HiOutlineUserGroup} active={false} />
+          <HeaderIcons Icon={IoLogoGameControllerB} active={false} />
+        </div>
       </div>
 
       {/* Rightside */}
---
+++ components/HeaderIcons.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  Icon: any;
+  active: boolean;
+}
+
+function HeaderIcons({ Icon, active }: Props) {
+  return (
+    <div className="flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 hover:border-b-4 active:border-b-4 border-blue-600 rounded group">
+      <Icon
+        className={`text-gray-600 h-5 text-center sm:h-7 mx-auto group-active:text-blue-600 ${
+          active && "text-blue-600"
+        }`}
+        size="1.5rem"
+      />
+    </div>
+  );
+}
+
+export default HeaderIcons;

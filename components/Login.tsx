6067e97e28587ba0711fa52fea7fad9a831d63d5
SaadEssam <SaadEssam059@gmail.com>
2023-04-10 17:51:09 +0200

    Restructure and style login component

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,37 +2,53 @@ import Image from "next/image";
 
 function Login() {
   return (
-    <div className="flex items-center justify-between h-screen px-10">
-      <div className="w-96 space-y-3 p-8">
+    <div className="flex items-center justify-between w-full h-screen p-10 bg-blue-50">
+      <div className="w-1/3 space-y-4 p-8">
         <Image
           src="https://i.postimg.cc/NMyj90t9/logo.png"
           alt="Facebook"
-          height={250}
-          width={250}
+          height={300}
+          width={300}
           objectFit="contain"
         />
-        <p className="font-semibold">
+        <p className="font-normal text-2xl">
           Facebook helps you connect and share with the people in your life.
         </p>
       </div>
-      <div className="flex flex-col bg-white shadow-lg p-8 rounded-lg">
-        <form action="" className="flex flex-col ">
-          <input
-            type="text"
-            placeholder="Email address or phone number"
-            className=""
-          />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Log In</button>
-          <a href="#" className="text-blue-600 hover:underline">
-            Forgotten Password
-          </a>
-        </form>
-        <hr className="w-2 h-2 text-gray-400" />
-        <div className="create-btn">
-          <a href="" target="_blank">
-            Create New Account
-          </a>
+      <div className="flex flex-col items-center justify-center h-screen w-5/12 ">
+        <div className="bg-white p-5 rounded-lg shadow-lg w-96">
+          <form className="flex flex-col ">
+            <input
+              type="email"
+              placeholder="Email address"
+              className="mb-4 py-3 px-3 rounded-sm border-gray-300 border focus:outline-none "
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="mb-4 py-3 px-3 rounded-sm border-gray-300 border focus:outline-none "
+              required
+            />
+            <button
+              type="submit"
+              className="py-3 px-3 bg-blue-600 text-white rounded-md font-medium hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-600"
+            >
+              Log In
+            </button>
+          </form>
+          <div className="flex flex-col mt-3 text-sm text-gray-500">
+            <a href="#" className="mx-auto text-blue-600 hover:underline">
+              Forgotten Password
+            </a>
+            <hr className="mt-3" />
+            <button
+              type="submit"
+              className="py-3 px-3 w-1/2 mx-auto mt-3 bg-green-600 text-white rounded-md font-medium hover:bg-green-700 focus:outline-none focus:ring focus:border-green-600"
+            >
+              Create New Account
+            </button>
+          </div>
         </div>
       </div>
     </div>

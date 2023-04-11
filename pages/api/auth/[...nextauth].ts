93136275bcc3a8fbe3b5ff65f52ed47e8528d24d
SaadEssam <SaadEssam059@gmail.com>
2023-04-11 02:08:58 +0200

    Fix: clientId, clientSecret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,9 +4,11 @@ import FacebookProvider from "next-auth/providers/facebook";
 export default NextAuth({
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_ID ?? "",
-      clientSecret: process.env.FACEBOOK_SECRET ?? "",
+      clientId: process.env.FACEBOOK_CLIENT_ID ?? "",
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? "",
     }),
     // ...add more providers here
   ],
 });
+
+

26becc0683d5997cd6dcd192dce38f9e244d5c3a
SaadEssam <SaadEssam059@gmail.com>
2023-04-12 00:33:32 +0200

    Fix: signIn function

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,17 +1,9 @@
-import { signIn, SignInResponse } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 
 function Login() {
-  const handleSignIn = async (): Promise<SignInResponse> => {
-    const response = await signIn("facebook");
-    if (response !== undefined) {
-      return response;
-    } else {
-      throw new Error("Sign in failed");
-    }
-    // return response;
-  };
-  
+  const { data: session } = useSession();
+
   return (
     <div className="flex sm:flex-col md:flex-row items-center justify-between w-full sm:h-full md:h-screen p-10 bg-blue-50">
       <div className="grid sm:place-items-center md:place-items-start sm:w-full md:w-1/3 space-y-4 p-8">
@@ -41,13 +33,19 @@ function Login() {
               className="mb-4 py-3 px-3 rounded-sm border-gray-300 border focus:outline-none"
               required
             /> */}
-            <button
-              type="button"
-              onClick={handleSignIn}
-              className="py-3 px-3 bg-blue-600 text-white rounded-md font-medium hover:bg-blue-500 focus:outline-none"
-            >
-              Log In
-            </button>
+
+            {!session && (
+              <button
+                type="button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+                className="py-3 px-3 bg-blue-600 text-white rounded-md font-medium hover:bg-blue-500 focus:outline-none"
+              >
+                Log In
+              </button>
+            )}
           </form>
           <div className="flex flex-col mt-4 text-sm">
             <a href="#" className="mx-auto text-blue-600 hover:underline">

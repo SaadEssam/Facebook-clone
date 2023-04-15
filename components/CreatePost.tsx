3f0c319d75bcb174b4283b2022e9bccdbcf28d4e
SaadEssam <SaadEssam059@gmail.com>
2023-04-15 18:14:15 +0200

    Build a CreatePost component

--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -4,6 +4,9 @@ import { Session } from "next-auth";
 import Image from "next/image";
 import { useState } from "react";
 
+import { BiHappy } from "react-icons/bi";
+import { FaPhotoVideo, FaVideo } from "react-icons/fa";
+
 function CreatePost() {
   const [showModal, setShowModal] = useState(false);
   const handleClick = () => {
@@ -23,16 +26,35 @@ function CreatePost() {
           layout="fixed"
         />
         <form className="flex flex-1">
-          <input
+          <button
             onClick={handleClick}
-            type="text"
-            placeholder={`What's on your mind, ${session?.user?.name}?`}
-            className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none font-poppins"
-          />
+            // placeholder={`What's on your mind, ${session?.user?.name}?`}
+            className="rounded-full h-12 bg-gray-100 hover:bg-gray-50 flex-grow px-1 focus:outline-none font-poppins"
+          >
+            <span className="flex items-start justify-start p-2">
+              {`What's on your mind, ${session?.user?.name}?`}
+            </span>
+          </button>
         </form>
       </div>
       <div className="px-4">
         <hr className="bg-gray-300" />
+      </div>
+      <div className="flex justify-evenly p-2 ">
+        <div className="inputIcon">
+          <FaVideo className="h-7 w-7 text-red-500" />
+          <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
+        </div>
+
+        <div className="inputIcon">
+          <FaPhotoVideo className="h-7 w-7 text-green-500" />
+          <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+        </div>
+
+        <div className="inputIcon">
+          <BiHappy className="h-7 w-7 text-yellow-500" />
+          <p className="text-xs sm:text-sm xl:text-base">Feeling/activity</p>
+        </div>
       </div>
       {/* <PostModal showModal={showModal} handleClick={handleClick} /> */}
     </div>

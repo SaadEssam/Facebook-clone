06a172bc13b240ce89da4b98dd8e248174c42055
SaadEssam <SaadEssam059@gmail.com>
2023-04-29 18:41:56 +0300

    Separate constants into the constants folder

---
+++ constants/contacts.ts
@@ -0,0 +1,32 @@
+export const contact = [
+  {
+    name: "Joseph Gonzalez",
+    profile:
+      "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
+  },
+  {
+    name: "Michel Peter",
+    profile:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  },
+  {
+    name: "Farshid Zabbahi",
+    profile:
+      "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  },
+  {
+    name: "Tirza van",
+    profile:
+      "https://images.unsplash.com/photo-1581824043583-6904b080a19c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=538&q=80",
+  },
+  {
+    name: "Josh Hild",
+    profile:
+      "https://images.unsplash.com/profile-1652754804106-1b1e8b7c0528image?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff",
+  },
+  {
+    name: "Ivana Cajina",
+    profile:
+      "https://images.unsplash.com/profile-1665026644009-c4369aba033dimage?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff",
+  },
+];

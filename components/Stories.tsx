9def036f0b282b9ebba2b4cc752db13ee288ef30
SaadEssam <SaadEssam059@gmail.com>
2023-05-01 17:01:25 +0300

    Add right arrow icon to see more stories

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,10 +1,11 @@
 import { stories } from "@/constants/stories";
 import StoryCard from "./StoryCard";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 function Stories() {
   return (
     <>
-      <div className="flex justify-center space-x-3 mx-auto">
+      <div className="flex justify-center relative space-x-3 mx-auto">
         {stories.map((story) => (
           <StoryCard
             key={story.src}
@@ -13,6 +14,9 @@ function Stories() {
             profile={story.profile}
           />
         ))}
+        <div className="w-12 h-12 rounded-full hidden lg:grid place-items-center text-4xl bg-white absolute -right-6 top-1/2 transform -translate-y-1/2 border border-gray-200 cursor-pointer hover:bg-gray-100 shadow text-gray-500">
+          <MdOutlineKeyboardArrowRight />
+        </div>
       </div>
       <hr className="my-3 bg-gray-300" />
     </>
